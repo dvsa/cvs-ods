830797827d823d18b891c3579374d8b08f7d777a
Thomas.Webb <SA_Thomas.Webb@dvsatest-cloud.uk>
2022-06-06 10:06:43 +0100

    fix(cb2-3185): fixed typo in sql view syntax

--- sql/00002_views.sql
+++ sql/00002_views.sql
@@ -1,3 +1,5 @@
+--liquibase formatted sql
+--changeset liquibase:create -multiple-tables:1 splitStatements:true endDelimiter:; context:dev
 CREATE VIEW evl_view AS 
     SELECT MAX(testExpiryDate) AS testExpiryDate, vrm_trm, certificateNumber
     FROM test_result t
@@ -11,7 +13,7 @@ CREATE VIEW evl_view AS
     WHERE 
         t.testExpiryDate > DATE(NOW() - INTERVAL 3 DAY) AND 
         (t.certificateNumber IS NOT NULL AND 
-        .certificateNumber != '' AND 
+        t.certificateNumber != '' AND 
         NOT LOCATE(' ', t.certificateNumber) > 0) AND
         SubQ.vrm_trm NOT REGEXP '^[a-zA-Z][0-9]{6}$'
     GROUP BY vrm_trm, certificateNumber;
