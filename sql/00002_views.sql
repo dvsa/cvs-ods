c0558ff31c4b498554e1100bb8cc1bb9c2176145
Reise <reise.dawson@bjss.com>
2022-06-14 11:53:28 +0100

    changing view creation to OR REPLACE, adding annual certificate filter and moving REGEX inside subQ

--- sql/00002_views.sql
+++ sql/00002_views.sql
@@ -1,19 +1,28 @@
 --liquibase formatted sql
 --changeset liquibase:create -multiple-tables:1 splitStatements:true endDelimiter:; context:dev
-CREATE VIEW evl_view AS 
-    SELECT MAX(testExpiryDate) AS testExpiryDate, vrm_trm, certificateNumber
-    FROM test_result t
-    LEFT JOIN (SELECT MAX(createdAt), id, vrm_trm 
-        FROM vehicle 
-        WHERE
-            LENGTH(vrm_trm) < 8 AND 
-            vrm_trm NOT REGEXP '^[0-9]{6}[zZ]$'
-            GROUP BY id, vrm_trm
-    ) SubQ ON SubQ.id=t.vehicle_id
-    WHERE 
-        t.testExpiryDate > DATE(NOW() - INTERVAL 3 DAY) AND 
-        (t.certificateNumber IS NOT NULL AND 
-        t.certificateNumber != '' AND 
-        NOT LOCATE(' ', t.certificateNumber) > 0) AND
-        SubQ.vrm_trm NOT REGEXP '^[a-zA-Z][0-9]{6}$'
-    GROUP BY vrm_trm, certificateNumber;
+CREATE OR REPLACE VIEW evl_view AS
+SELECT MAX(testExpiryDate) AS testExpiryDate,
+    SubQ.vrm_trm,
+    t.certificateNumber
+FROM test_result t
+    JOIN test_type tt ON t.test_type_id = tt.id
+    JOIN (
+        SELECT MAX(createdAt),
+            id,
+            vrm_trm
+        FROM vehicle
+        WHERE LENGTH(vrm_trm) < 8
+            AND vrm_trm NOT REGEXP '^[a-zA-Z][0-9]{6}$'
+            AND vrm_trm NOT REGEXP '^[0-9]{6}[zZ]$'
+        GROUP BY id,
+            vrm_trm
+    ) SubQ ON SubQ.id = t.vehicle_id
+WHERE t.testExpiryDate > DATE(NOW() - INTERVAL 3 DAY)
+    AND (
+        t.certificateNumber IS NOT NULL
+        AND t.certificateNumber != ''
+        AND NOT LOCATE(' ', t.certificateNumber) > 0
+    )
+    AND tt.testTypeClassification = 'Annual With Certificate'
+GROUP BY SubQ.vrm_trm,
+    t.certificateNumber;
