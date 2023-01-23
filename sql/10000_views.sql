22f48bae3afefd4a6b300ffdf59e741f83db5a25
Guillaume Julien <guillaume.julien@bjss.com>
2023-01-23 16:54:20 +0000

    feat(cb2-7228): filtering business

--- sql/10000_views.sql
+++ sql/10000_views.sql
@@ -1,16 +1,37 @@
 --liquibase formatted sql
 --changeset liquibase:3 -multiple-tables:1 splitStatements:true endDelimiter:; context:dev
-CREATE OR REPLACE VIEW evl_view AS
-SELECT testExpiryDate, vrm_trm, certificateNumber 
-FROM (
-	SELECT testExpiryDate, vrm_trm, certificateNumber, IF(@prev <> vrm_trm, @rn:=0,@rn) as row_number_over_partition, @prev:=vrm_trm, @rn:=@rn+1 AS rn
-	FROM ( 
-		SELECT MAX(testExpiryDate) AS testExpiryDate,
-			SubQ.vrm_trm,
-			t.certificateNumber,
-			t.testTypeEndTimeStamp
-		FROM test_result as t
-			JOIN test_type tt ON t.test_type_id = tt.id
+-- Refactor to use ROW_NUMBER() OVER(PARTITION ...) ONCE MIGRATED TO MYSQL 8.0+
+CREATE OR REPLACE VIEW evl_view AS 
+SELECT MAX(testExpiryDate) AS testExpiryDate,
+	SubQ.vrm_trm,
+	t.certificateNumber
+FROM test_result as t
+	JOIN test_type tt ON t.test_type_id = tt.id
+	JOIN (
+		SELECT MAX(createdAt),
+			id,
+			vrm_trm
+		FROM vehicle
+		WHERE LENGTH(vrm_trm) < 8
+			AND vrm_trm NOT REGEXP '^[a-zA-Z][0-9]{6}$'
+			AND vrm_trm NOT REGEXP '^[0-9]{6}[zZ]$'
+		GROUP BY id,
+			vrm_trm
+	) SubQ ON SubQ.id = t.vehicle_id
+WHERE t.testExpiryDate > DATE(NOW() - INTERVAL 3 DAY)
+                    AND (
+                        t.certificateNumber IS NOT NULL
+                        AND t.certificateNumber != ''
+                        AND NOT LOCATE(' ', t.certificateNumber) > 0
+                    )
+			AND tt.testTypeClassification = 'Annual With Certificate'
+    AND NOT EXISTS (
+		SELECT 1 FROM test_result
+		WHERE t.vehicle_id = test_result.vehicle_id AND t.certificateNumber < test_result.certificateNumber
+    )
+	AND (testExpiryDate, vrm_trm, testTypeEndTimestamp) IN (
+		SELECT DISTINCT testExpiryDate, vrm_trm, MAX(testTypeEndTimeStamp) AS testTypeEndTimeStamp
+			FROM test_result tr
 			JOIN (
 				SELECT MAX(createdAt),
 					id,
@@ -21,18 +42,24 @@ FROM (
 					AND vrm_trm NOT REGEXP '^[0-9]{6}[zZ]$'
 				GROUP BY id,
 					vrm_trm
-			) SubQ ON SubQ.id = t.vehicle_id
-		WHERE t.testExpiryDate > DATE(NOW() - INTERVAL 3 DAY)
-            AND (
-                t.certificateNumber IS NOT NULL
-                AND t.certificateNumber != ''
-                AND NOT LOCATE(' ', t.certificateNumber) > 0
-            )
-			AND tt.testTypeClassification = 'Annual With Certificate'
-		GROUP BY SubQ.vrm_trm,
-			t.certificateNumber,
-			t.testTypeEndTimeStamp
-		ORDER BY vrm_trm, testExpiryDate DESC, t.testTypeEndTimeStamp DESC
-	) as SubQ2
-) as SubQ3
-WHERE row_number_over_partition = 0
+			) SubQ ON SubQ.id = tr.vehicle_id
+			WHERE (vrm_trm, testExpiryDate) IN (
+				SELECT vrm_trm, MAX(t.testExpiryDate) AS testExpiryDate
+				 FROM test_result t
+				 JOIN (
+					SELECT MAX(createdAt),
+						id,
+						vrm_trm
+					FROM vehicle
+					WHERE LENGTH(vrm_trm) < 8
+						AND vrm_trm NOT REGEXP '^[a-zA-Z][0-9]{6}$'
+						AND vrm_trm NOT REGEXP '^[0-9]{6}[zZ]$'
+					GROUP BY id,
+						vrm_trm
+				) SubQ ON SubQ.id = t.vehicle_id
+				GROUP BY vrm_trm
+		    )
+		GROUP BY vrm_trm, testExpiryDate
+	)	
+GROUP BY SubQ.vrm_trm,
+	t.certificateNumber
