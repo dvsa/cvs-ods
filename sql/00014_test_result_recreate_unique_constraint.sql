c83801d157cfe034d87049308fac7e29b954ef30
Chris Peska <chris.peska@bjss.com>
2023-01-26 14:05:50 +0000

    fix(noptest-2): added missing attribute to test_result unique key

--- sql/00014_test_result_recreate_unique_constraint.sql
+++ sql/00014_test_result_recreate_unique_constraint.sql
@@ -1,4 +1,4 @@
 --liquibase formatted sql
 --changeset liquibase:create -multiple-tables:1 splitStatements:true endDelimiter:; context:dev
 ALTER TABLE `test_result` DROP INDEX `idx_comp_test_result_uq`;
-ALTER TABLE `test_result` ADD UNIQUE `idx_comp_test_result_uq` (`vehicle_id`, `testResultId`, `testtype_fingerprint`);
+ALTER TABLE `test_result` ADD UNIQUE `idx_comp_test_result_uq` (`vehicle_id`, `testResultId`, `fuel_emission_id`, `testtype_fingerprint`);
