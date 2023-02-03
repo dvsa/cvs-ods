e24b66b416f79d78edbd0585995f0a1b955597c1
ian-hodges <ian.hodges@bjss.com>
2023-02-03 16:31:43 +0000

    feat(cb2-7492): update datetime colums to datetime(6)

--- sql/00012_extend_datetime_columns.sql
+++ sql/00012_extend_datetime_columns.sql
@@ -1,9 +1,14 @@
 --liquibase formatted sql
 --changeset liquibase:2 -multiple-tables:1 endDelimiter:;
-ALTER TABLE vehicle MODIFY createdAt DATETIME(6), ALGORITHM=COPY, LOCK=SHARED;
-ALTER TABLE technical_record MODIFY createdAt DATETIME(6), ALGORITHM=COPY, LOCK=SHARED;
-ALTER TABLE technical_record MODIFY lastUpdatedAt DATETIME(6), ALGORITHM=COPY, LOCK=SHARED;
-ALTER TABLE test_result MODIFY createdAt DATETIME(6), ALGORITHM=COPY, LOCK=SHARED;
-ALTER TABLE test_result MODIFY lastUpdatedAt DATETIME(6), ALGORITHM=COPY, LOCK=SHARED;
-ALTER TABLE test_result MODIFY testTypeStartTimestamp DATETIME(6), ALGORITHM=COPY, LOCK=SHARED;
-ALTER TABLE test_result MODIFY testTypeEndTimestamp DATETIME(6), ALGORITHM=COPY, LOCK=SHARED;
+ALTER TABLE vehicle
+    MODIFY createdAt DATETIME(6), ALGORITHM=COPY, LOCK=SHARED;
+
+ALTER TABLE technical_record
+    MODIFY createdAt DATETIME(6),
+    MODIFY lastUpdatedAt DATETIME(6), ALGORITHM=COPY, LOCK=SHARED;
+
+ALTER TABLE test_result
+    MODIFY createdAt DATETIME(6),
+    MODIFY lastUpdatedAt DATETIME(6),
+    MODIFY testTypeStartTimestamp DATETIME(6),
+    MODIFY testTypeEndTimestamp DATETIME(6), ALGORITHM=COPY, LOCK=SHARED;
