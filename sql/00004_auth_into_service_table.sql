f289d29c6fd01c8b4e6dc3748384decdb2993c32
Ian Hodges <doctorhodges@gmail.com>
2023-03-17 08:15:33 +0000

    feat(cb2-7792): save authIntoService to the database

---
+++ sql/00004_auth_into_service_table.sql
@@ -0,0 +1,16 @@
+--liquibase formatted sql
+--changeset liquibase:create -multiple-tables:1 splitStatements:true endDelimiter:// context:dev
+CREATE TABLE `auth_into_service` (
+  `id`                  bigint unsigned NOT NULL AUTO_INCREMENT,
+  `technical_record_id` bigint unsigned NOT NULL,
+  `cocIssueDate`        datetime DEFAULT NULL,
+  `dateReceived`        datetime DEFAULT NULL,
+  `datePending`         datetime DEFAULT NULL,
+  `dateAuthorised`      datetime DEFAULT NULL,
+  `dateRejected`        datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_technical_record_auth_into_service_id_uq` (`technical_record_id`),
+  CONSTRAINT `auth_into_service_ibfk_1`
+    FOREIGN KEY (`technical_record_id`)
+    REFERENCES `technical_record` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB
