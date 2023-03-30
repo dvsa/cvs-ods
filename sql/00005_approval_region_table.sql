721a85ea60f045b2b410e5893d44eeb6ef34d504
Ian Hodges <doctorhodges@gmail.com>
2023-03-30 15:51:40 +0100

    feat(cb2-7793): add approval region lookup table

--- sql/00005_approval_region_table.sql
+++ sql/00005_approval_region_table.sql
@@ -4,7 +4,7 @@
 CREATE TABLE IF NOT EXISTS `approval_region` (
   `id`                  bigint unsigned NOT NULL AUTO_INCREMENT,
   `approvalType`        varchar(50) NOT NULL,
-  `approvalregion`      varchar(50) NOT NULL,
+  `approvalRegion`      varchar(50) NOT NULL,
   `fromDate`            date,
   `toDate`              date,
   PRIMARY KEY (`id`)
