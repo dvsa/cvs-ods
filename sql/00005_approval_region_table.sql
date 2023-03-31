055261218188e06e558deb3868da2e1e25f5391d
Ian Hodges <ian.hodges@bjss.com>
2023-03-31 13:46:43 +0100

    CB2-7793: Add approval region lookup table (#19)
    * feat(cb2-7793): add approval region lookup table

---
+++ sql/00005_approval_region_table.sql
@@ -0,0 +1,11 @@
+--liquibase formatted sql
+--changeset liquibase:create -multiple-tables:1 splitStatements:true endDelimiter:; context:dev
+
+CREATE TABLE IF NOT EXISTS `approval_region` (
+  `id`                  bigint unsigned NOT NULL AUTO_INCREMENT,
+  `approvalType`        varchar(50) NOT NULL,
+  `approvalRegion`      varchar(50) NOT NULL,
+  `fromDate`            date,
+  `toDate`              date,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
