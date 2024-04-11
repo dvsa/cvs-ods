edf64beaf0e0272dd263fde41352da37b13e4a2a
LGin-BJSS <leo.ginever@bjss.com>
2024-04-11 09:22:10 +0100

    Revert "feature/CB2-11231 - Change the accepted maximum length of the Plate Serial Number in NOP (#37)"
    This reverts commit cd8c830810e9450f8ae1229d1fe15854c43aa778.

--- sql/00009_alter_plateSerialNumber_field.sql
+++
@@ -1,4 +0,0 @@
---liquibase formatted sql
---changeset liquibase:modifyDataType -multiple-tables:1 splitStatements:true endDelimiter:; context:dev
-
-ALTER TABLE plate MODIFY plateSerialNumber VARCHAR(50), ALGORITHM=INPLACE, LOCK=NONE;
