c9ac56df21ab83126b109263d74dce7edddac51f
Simon Tindall <simon.tindall@bjss.com>
2025-01-23 14:00:07 +0000

    Added view for DVLA ANTS report

--- sql/10000_views.sql
+++ sql/10000_views.sql
@@ -101,4 +101,103 @@ final_dataset AS (
 SELECT
 	*
 FROM
-	final_dataset
+	final_dataset;
+
+
+CREATE OR REPLACE VIEW vw_dvla_ants AS
+
+WITH na_test_type_id AS (
+	SELECT	id
+	FROM 	CVSNOP.test_type
+	WHERE 	testTypeName LIKE '%notifiable alteration%'
+    AND		(testTypeName LIKE '%VTG10%' OR testTypeName LIKE '%VTG790%')
+),
+
+ranked_tests AS
+(
+	SELECT	vehicle_id,
+			createdAt,
+			RANK() OVER(partition by vehicle_id order by createdAt desc) sort_order
+	FROM	CVSNOP.test_result
+	WHERE	test_type_id IN (SELECT id FROM na_test_type_id)
+    AND		testResult = 'pass'
+),
+
+most_recent_test AS (
+	SELECT	*
+	FROM 	ranked_tests
+	WHERE	sort_order = 1
+),
+
+tech_records_before_test AS (
+	SELECT	tech.id,
+			tech.vehicle_id,
+			tech.createdAt,
+            tech.grossGbWeight,
+            tech.trainGbWeight,
+            vc.vehicleConfiguration,
+			RANK() OVER(PARTITION BY vehicle_id ORDER BY tech.createdAt DESC) tech_sort_order_before_test
+	FROM	technical_record AS tech
+	JOIN	vehicle_class AS vc
+	ON 		tech.vehicle_class_id = vc.id
+	JOIN 	most_recent_test AS mrt
+	ON		tech.vehicle_id = mrt.vehicle_id
+	WHERE	tech.statusCode in ('current','archived')
+	AND		tech.createdAt < mrt.createdAt
+),
+
+vehicle_timeline AS (
+	SELECT	mrt.vehicle_id,
+			mrt.createdAt 							AS test_result_createdAt,
+
+            init.id 								AS initial_tech_record_id,
+            init.createdAt 							AS initial_tech_record_createdAt,
+			COALESCE(init.grossGbWeight,0)			AS initial_grossGbWeight,
+            COALESCE(init.trainGbWeight,0)			AS initial_trainGbWeight,
+
+            prov.id 								AS provisional_tech_record_id,
+			prov.createdAt 							AS provisional_tech_record_createdAt,
+			COALESCE(prov.grossGbWeight,0)			AS provisional_grossGbWeight,
+            COALESCE(prov.trainGbWeight,0)			AS provisional_trainGbWeight,
+			prov.vehicleConfiguration				AS provisional_vehicleConfiguration
+	FROM	most_recent_test mrt
+	JOIN	tech_records_before_test init 			ON mrt.vehicle_id = init.vehicle_id
+													AND init.tech_sort_order_before_test = 2
+	JOIN	tech_records_before_test prov 			ON mrt.vehicle_id = prov.vehicle_id
+													AND prov.tech_sort_order_before_test = 1
+),
+
+final_dataset AS(
+	SELECT 	v.vrm_trm,
+			NULL AS make,
+			NULL AS model,
+			NULL as wheelplan,
+			CASE
+				WHEN provisional_vehicleConfiguration = 'rigid' THEN initial_grossGbWeight
+				WHEN provisional_vehicleConfiguration = 'articulated' THEN initial_trainGbWeight
+			END AS gross_weight_before_test,
+			CASE
+				WHEN provisional_vehicleConfiguration = 'rigid' THEN provisional_grossGbWeight
+				WHEN provisional_vehicleConfiguration = 'articulated' THEN provisional_trainGbWeight
+			END AS gross_weight_after_test,
+			'1111' AS DOE_reference,
+			NULL AS date_of_plating,
+			provisional_tech_record_createdAt AS tech_record_creation_date
+	FROM 	vehicle_timeline vt
+	JOIN	vehicle v on vt.vehicle_id = v.id
+
+	WHERE
+			CASE
+				WHEN provisional_vehicleConfiguration = 'rigid'
+						AND initial_grossGbWeight <> provisional_grossGbWeight
+						THEN TRUE
+				WHEN provisional_vehicleConfiguration = 'articulated'
+						AND initial_trainGbWeight <> provisional_trainGbWeight
+						THEN TRUE
+				ELSE FALSE
+			END = TRUE
+	order by test_result_createdAt desc
+)
+
+SELECT *
+FROM final_dataset;
